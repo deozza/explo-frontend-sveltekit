c31ce022623012e68b88cbdf0696fe82c180404e
deozza <edenn.touitou@letsjam.fr>
2024-04-02 23:08:29 +0200

    created component

---
+++ src/lib/Header.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+
+</script>
+
+<h1>Title from component</h1>
+
+<style>
+    
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script lang="ts">
+
+import Header from "$lib/Header.svelte";
+
+</script>
+
+<Header />
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+    
+</style>
