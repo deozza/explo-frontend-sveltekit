4201809dd3e0d0adca8075e87fbd67ae7d004c60
deozza <edenn.touitou@letsjam.fr>
2024-04-03 00:40:39 +0200

    bind

---
+++ src/lib/Sum.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    export let a: number;
+    export let b: number;
+    export let result: number;
+    
+    function sum(a: number, b: number): number {
+        return a + b;
+    }
+</script>
+
+<div>
+
+    <input type="number" bind:value={a}>
+    <input type="number" bind:value={b}>
+    <button on:click={() => result = sum(a, b)}>Sum</button>
+</div>
