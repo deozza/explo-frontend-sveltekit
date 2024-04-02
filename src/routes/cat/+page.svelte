e89c666009bdbe1a6227edea220727517f0c786f
deozza <edenn.touitou@letsjam.fr>
2024-04-03 00:10:21 +0200

    variable done

---
+++ src/routes/cat/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+    let catUrl: string = '';
+
+    onMount(() => {
+        fetch('https://api.thecatapi.com/v1/images/search')
+        .then(response => response.json())
+        .then(data => {
+            catUrl = data[0].url;
+        });
+    });
+
+</script>
+
+<h1>Cat</h1>
+
+<img src="{catUrl}" alt="cat">
+
+<style>
+
+</style>
