27edad30e6bfd35a015116c768bbbd613bb7153e
deozza <edenn.touitou@letsjam.fr>
2024-04-03 00:52:00 +0200

    event

---
+++ src/routes/count/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    let count: number = 0;
+
+    function increment() {
+        count += 1;
+    }
+
+    function decrement() {
+        count -= 1;
+    }
+
+</script>
+
+<h1>Count</h1>
+
+<button on:click={decrement}>-</button>
+<p>Count: {count}</p>
+<button on:click={increment}>+</button>
