c6e0dda57d183c24b1e7914cfd7936ca58ba1252
deozza <edenn.touitou@letsjam.fr>
2024-04-03 11:43:31 +0200

    Add layout components for navigation and footer

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<nav>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/await">Await</a></li>
+        <li><a href="/cat">Cat</a></li>
+        <li><a href="/count">Count</a></li>
+        <li><a href="/reactive">Reactive</a></li>
+        <li><a href="/sum">Sum</a></li>
+        <li><a href="/todo">Todo</a></li>
+
+    </ul>
+</nav>
+
+<main>
+    <slot></slot>
+</main>
