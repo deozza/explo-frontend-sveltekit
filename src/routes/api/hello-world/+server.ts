ed0276fe08d1f21071b91b3f478cc0af5055563d
deozza <edenn.touitou@letsjam.fr>
2024-04-03 12:13:15 +0200

    Update user authentication logic***

---
+++ src/routes/api/hello-world/+server.ts
@@ -0,0 +1,9 @@
+import type { RequestHandler } from './$types';
+import { json } from '@sveltejs/kit'
+
+
+export const GET: RequestHandler = async () => {
+    
+    
+      return json({ 'hello': 'world' })
+};
